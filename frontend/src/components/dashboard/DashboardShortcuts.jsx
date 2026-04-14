0043d41537b2793eb2b245217f9cd6c240c7f5b2
aidilmslm <amus790@aucklanduni.ac.nz>
2026-04-14 15:12:19 +1200

    connect the modal to the icons. inventory icon and modal on progress

--- frontend/src/components/dashboard/DashboardShortcuts.jsx
+++ frontend/src/components/dashboard/DashboardShortcuts.jsx
@@ -17,11 +17,24 @@ function DashboardShortcuts({ openModal, modalType, closeModal }) {
             </button>
             <button onClick={() => toggle('mytask')}>
                 <img src="public/dashboardicons/scroll_freepik.png" alt="My Task" />
+function DashboardShortcuts({ openModal }) {
+
+    return (
+        <div className="dashboard-shortcuts">
+            <button onClick={() => openModal('p2p')}>
+                <img src="public/dashboardicons/board-1.png" alt="Public Task" />
+            </button>
+            <button onClick ={() => openModal('community')}>
+                <img src="public/dashboardicons/calendar_pixel_perfect.png" alt="Public Task" />
+            </button>
+            <button onClick={() => openModal('mytask')}>
+                <img src="public/dashboardicons/scroll_freepik.png" alt="Public Task" />
             </button>
             <button onClick={() => toggle('inventory')}>
                 <img src="public/dashboardicons/backpack_freepik.png" alt="Inventory" />
             </button>
             <button onClick={() => toggle('store')}>
+            <button onClick={() => openModal('store')}>
                 <img src="public/dashboardicons/shop_goerge_cresnar.png" alt="Store" />
             </button>
         </div>
