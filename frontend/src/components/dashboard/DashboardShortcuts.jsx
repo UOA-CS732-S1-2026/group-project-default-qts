7fa66a0daef03a0d1d7110e9a6adf6f0df696a05
aidilmslm <amus790@aucklanduni.ac.nz>
2026-05-05 11:13:34 +1200

    invert shortcut color when the dark theme on

--- frontend/src/components/dashboard/DashboardShortcuts.jsx
+++ frontend/src/components/dashboard/DashboardShortcuts.jsx
@@ -3,20 +3,20 @@ import '@/styles/dashboard/DashboardShortcuts.css';
 function DashboardShortcuts({ openModal, modalType, closeModal }) {
     return (
         <div className="dashboard-shortcuts">
-            <button onClick={() => openModal('p2p')}>
-                <img src="public/dashboardicons/board-1.png" alt="P2P Task" />
+            <button className="shortcut-btn" onClick={() => openModal('p2p')}>
+                <img className="shortcut-icon" src="public/dashboardicons/board-1.png" alt="P2P Task" />
             </button>
-            <button onClick={() => openModal('community')}>
-                <img src="public/dashboardicons/calendar_pixel_perfect.png" alt="Community Task" />
+            <button className="shortcut-btn" onClick={() => openModal('community')}>
+                <img className="shortcut-icon" src="public/dashboardicons/calendar_pixel_perfect.png" alt="Community Task" />
             </button>
-            <button onClick={() => openModal('mytask')}>
-                <img src="public/dashboardicons/scroll_freepik.png" alt="My Task" />
+            <button className="shortcut-btn" onClick={() => openModal('mytask')}>
+                <img className="shortcut-icon" src="public/dashboardicons/scroll_freepik.png" alt="My Task" />
             </button>
-            <button onClick={() => openModal('inventory')}>
-                <img src="public/dashboardicons/backpack_freepik.png" alt="Inventory" />
+            <button className="shortcut-btn" onClick={() => openModal('inventory')}>
+                <img className="shortcut-icon" src="public/dashboardicons/backpack_freepik.png" alt="Inventory" />
             </button>
-            <button onClick={() => openModal('store')}>
-                <img src="public/dashboardicons/shop_goerge_cresnar.png" alt="Store" />
+            <button className="shortcut-btn" onClick={() => openModal('store')}>
+                <img className="shortcut-icon" src="public/dashboardicons/shop_goerge_cresnar.png" alt="Store" />
             </button>
         </div>
     )
