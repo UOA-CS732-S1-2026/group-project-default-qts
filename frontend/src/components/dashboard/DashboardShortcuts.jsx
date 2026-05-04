0e3b2a339c1f772c1693264d046900539130fc14
aidilmslm <amus790@aucklanduni.ac.nz>
2026-05-05 11:19:11 +1200

    fix public path warning

--- frontend/src/components/dashboard/DashboardShortcuts.jsx
+++ frontend/src/components/dashboard/DashboardShortcuts.jsx
@@ -4,19 +4,19 @@ function DashboardShortcuts({ openModal, modalType, closeModal }) {
     return (
         <div className="dashboard-shortcuts">
             <button className="shortcut-btn" onClick={() => openModal('p2p')}>
-                <img className="shortcut-icon" src="public/dashboardicons/board-1.png" alt="P2P Task" />
+                <img className="shortcut-icon" src="/dashboardicons/board-1.png" alt="P2P Task" />
             </button>
             <button className="shortcut-btn" onClick={() => openModal('community')}>
-                <img className="shortcut-icon" src="public/dashboardicons/calendar_pixel_perfect.png" alt="Community Task" />
+                <img className="shortcut-icon" src="/dashboardicons/calendar_pixel_perfect.png" alt="Community Task" />
             </button>
             <button className="shortcut-btn" onClick={() => openModal('mytask')}>
-                <img className="shortcut-icon" src="public/dashboardicons/scroll_freepik.png" alt="My Task" />
+                <img className="shortcut-icon" src="/dashboardicons/scroll_freepik.png" alt="My Task" />
             </button>
             <button className="shortcut-btn" onClick={() => openModal('inventory')}>
-                <img className="shortcut-icon" src="public/dashboardicons/backpack_freepik.png" alt="Inventory" />
+                <img className="shortcut-icon" src="/dashboardicons/backpack_freepik.png" alt="Inventory" />
             </button>
             <button className="shortcut-btn" onClick={() => openModal('store')}>
-                <img className="shortcut-icon" src="public/dashboardicons/shop_goerge_cresnar.png" alt="Store" />
+                <img className="shortcut-icon" src="/dashboardicons/shop_goerge_cresnar.png" alt="Store" />
             </button>
         </div>
     )
